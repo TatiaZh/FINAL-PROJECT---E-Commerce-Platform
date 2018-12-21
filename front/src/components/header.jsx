591a27a07bce5dbda806c7a230c781797d19cc42
MAnjaparidze <anjaparidzemamuka@gmail.com>
2018-12-21 22:53:33 +0400

    Upgraded Home - Page

--- front/src/components/header.jsx
+++ front/src/components/header.jsx
@@ -6,28 +6,31 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <div className="header__inner">
-          <nav>
-            <ul>
-              <li>
-                {" "}
-                <a href="">Home</a>{" "}
-              </li>
-              <li>
-                {" "}
-                <a href="">Shop</a>{" "}
-              </li>
-              <li>
-                {" "}
-                <a href="">Blog</a>{" "}
-              </li>
-            </ul>
-          </nav>
-          <div className="header__inner__logo">
-            <img src={Logo} alt="BRINE" />
-          </div>
-          <div className="header__inner__cart">
-            <i className="fa fa-shopping-cart" />
+        <div className="header__inner--wrapper">
+          <div className="header__inner">
+            <nav>
+              <ul>
+                <li>
+                  {" "}
+                  <a href="">Home</a>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <a href="">Shop</a>{" "}
+                </li>
+                <li>
+                  {" "}
+                  <a href="">Blog</a>{" "}
+                </li>
+              </ul>
+            </nav>
+            <div className="header__inner__logo">
+              <img src={Logo} alt="BRINE" />
+            </div>
+            <div className="header__inner__cart">
+              <i className="fa fa-shopping-cart" />
+              <span className="item-count__div">0</span>
+            </div>
           </div>
         </div>
       </header>
